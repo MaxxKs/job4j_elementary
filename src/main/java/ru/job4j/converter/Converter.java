b3da6500d1ea34372a1d141a879193ca56a29e3a
MaxxKs <maks-ksenzow@mail.ru>
2024-07-23 12:33:14 +0300

    0. Что такое тестирование [#159431]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -2,47 +2,65 @@ package ru.job4j.converter;
 
 public class Converter {
     public static float rubleToEuro(float value) {
-        float result = value / 70;
-        return result;
+        return value / 70;
     }
 
     public static float euroToRuble(float value) {
-        float result = value * 70;
-        return result;
+        return value * 70;
     }
 
     public static float rubleToDollar(float value) {
-        float result = value / 60;
-        return result;
+        return value / 60;
     }
 
     public static float dollarToRuble(float value) {
-        float result = value * 60;
-        return result;
+        return value * 60;
     }
 
     public static float euroToDollar(float value) {
-        float result = value * 1.167F;
-        return result;
+        return value * 1.167F;
     }
 
     public static float dollarToEuro(float value) {
-        float result = value * 0.857F;
-        return result;
+        return value * 0.857F;
     }
 
     public static void main(String[] args) {
         float rubEur = rubleToEuro(140);
-        System.out.println("140 rubles are " + rubEur + " euro.");
+        float inputRubEur = 140;
+        float expectedRubEur = 2;
+        float outputRubEur = rubleToEuro(inputRubEur);
+        boolean passedRubEur = expectedRubEur == outputRubEur;
+        System.out.println("140 rubles are " + rubEur + " euro. Test result : " + passedRubEur);
         float eurRub = euroToRuble(2);
-        System.out.println("2 euro are " + eurRub + " rubles.");
+        float inputEurRub = 2;
+        float expectedEurRub = 140;
+        float outputEurRub = euroToRuble(inputEurRub);
+        boolean passedEurRub = expectedEurRub == outputEurRub;
+        System.out.println("2 euro are " + eurRub + " rubles. Test result : " + passedEurRub);
         float rubUsd = rubleToDollar(120);
-        System.out.println("120 rubles are " + rubUsd + " dollar.");
+        float inpetRubUsd = 120;
+        float expectedRubUsd = 2;
+        float outputRubUsd = rubleToDollar(inpetRubUsd);
+        boolean passedRubUsd = expectedRubUsd == outputRubUsd;
+        System.out.println("120 rubles are " + rubUsd + " dollar. Test result: " + passedRubUsd);
         float usdRub = dollarToRuble(2);
-        System.out.println("2 dollar are " + usdRub + " rubles.");
-        float eurUsd = euroToDollar(20);
-        System.out.println("20 euro are " + eurUsd + " dollar.");
-        float usdEur = dollarToEuro(20);
-        System.out.println("20 dollar are " + usdEur + " euro.");
+        float inputUsdRub = 2;
+        float expectedUsdRub = 120;
+        float outputUsdRub = dollarToRuble(inputUsdRub);
+        boolean passedUsdRub = expectedUsdRub == outputUsdRub;
+        System.out.println("2 dollar are " + usdRub + " rubles. Test result: " + passedUsdRub);
+        float eurUsd = euroToDollar(1000);
+        float inputEurUsd = 1000;
+        float expecedEurUsd = 1167;
+        float outputEurUsd = euroToDollar(inputEurUsd);
+        boolean passedEurUsd = expecedEurUsd == outputEurUsd;
+        System.out.println("1000 euro are " + eurUsd + " dollar. Test result: " + passedEurUsd);
+        float usdEur = dollarToEuro(1000);
+        float inputUsdEur = 1000;
+        float expectedUsdEur = 857;
+        float outputUsdEur = dollarToEuro(inputUsdEur);
+        boolean passedUsdEur = expectedUsdEur == outputUsdEur;
+        System.out.println("1000 dollar are " + usdEur + " euro. Test result: " + passedUsdEur);
     }
 }
