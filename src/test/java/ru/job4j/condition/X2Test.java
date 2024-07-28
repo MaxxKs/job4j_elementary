85f633d47233b62885f5043af1ac04e6857cb1a9
MaxxKs <maks-ksenzow@mail.ru>
2024-07-28 14:23:07 +0300

    1.2.0 Входные данные в тесте [#297750]

--- src/test/java/ru/job4j/condition/X2Test.java
+++ src/test/java/ru/job4j/condition/X2Test.java
@@ -3,7 +3,6 @@ package ru.job4j.condition;
 import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
-
 class X2Test {
 
     @Test
@@ -17,14 +16,14 @@ class X2Test {
         assertThat(result).isEqualTo(expected);
     }
 
-    @ Test
+    @Test
     void whenA1B1C1X1Then3() {
         int a = 1;
         int b = 1;
         int c = 1;
         int x = 1;
         int expected = 3;
-        int result = X2.calc(a, b , c, x);
+        int result = X2.calc(a, b, c, x);
         assertThat(result).isEqualTo(expected);
     }
 
