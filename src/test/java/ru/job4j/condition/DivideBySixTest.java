45236e581e9fe7b321285bcdaf115b1e6b2b36d3
MaxxKs <maks-ksenzow@mail.ru>
2024-08-09 13:31:12 +0300

    4.4. Вложенный оператор if else. Запутанный код [#190.0]

--- src/test/java/ru/job4j/condition/DivideBySixTest.java
+++ src/test/java/ru/job4j/condition/DivideBySixTest.java
@@ -9,31 +9,31 @@ class DivideBySixTest {
     void whenNumberDivideBy6() {
         int input = 24;
         String result = DivideBySix.checkNumber(input);
-        String expendet = "The number divides by 6.";
-        assertThat(result).isEqualTo(expendet);
+        String expected = "The number divides by 6.";
+        assertThat(result).isEqualTo(expected);
     }
 
     @Test
     void whenNumberDivideBy3AndNotEven() {
         int input = 9;
         String result = DivideBySix.checkNumber(input);
-        String expendet = "The number divides by 3, but it isn't the even number.";
-        assertThat(result).isEqualTo(expendet);
+        String expected = "The number divides by 3, but it isn't the even number.";
+        assertThat(result).isEqualTo(expected);
     }
 
     @Test
     void whenNumberNoDivideBy3AndEven() {
         int input = 14;
         String result = DivideBySix.checkNumber(input);
-        String expendet = "The number doesn't divide by 3, but it is the even number.";
-        assertThat(result).isEqualTo(expendet);
+        String expected = "The number doesn't divide by 3, but it is the even number.";
+        assertThat(result).isEqualTo(expected);
     }
 
     @Test
     void whenNumberNoDivideBy3AndNotEven() {
         int input = 25;
         String result = DivideBySix.checkNumber(input);
-        String expendet = "The number doesn't divide by 3 and it isn't the even number.";
-        assertThat(result).isEqualTo(expendet);
+        String expected = "The number doesn't divide by 3 and it isn't the even number.";
+        assertThat(result).isEqualTo(expected);
     }
 }
