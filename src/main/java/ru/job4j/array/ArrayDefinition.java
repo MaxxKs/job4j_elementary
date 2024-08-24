1ea96de996402dfd11499130725ee9b90fcde831
MaxxKs <maks-ksenzow@mail.ru>
2024-08-24 19:56:04 +0300

    6.0.3. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,16 @@ public class ArrayDefinition {
         System.out.println("Размер массива равен: " + surnames.length);
         float[] prices = new float[40];
         System.out.println("Размер массива равен: " + prices.length);
+        System.out.println();
+
+        String[] names = new String[4];
+        names[0] = "Sean Bean";
+        names[1] = "Michelle Fairley";
+        names[2] = "Richard Madden";
+        names[3] = "Sophie Turner";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
